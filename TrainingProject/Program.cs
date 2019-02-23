3a0296e4908e019a6b1d0125a082f335190a6392
Mateusz Marks <mmarks@pl.sii.eu>
2019-02-23 02:44:53 +0100

    bla bla

--- TrainingProject/Program.cs
+++ TrainingProject/Program.cs
@@ -10,7 +10,23 @@ namespace TrainingProject
     {
         static void Main(string[] args)
         {
+            //float a = 1.0f;
+            //float b = 3.0f;
+            //float c = a / b;
+            //double d = b * c;
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
 
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+            Console.ReadKey();
         }
     }
 }
